d8b2a60d720bed4964d05c12e90f4bd6f7cdc40f
drew <drewmcneely@email.arizona.edu>
2020-03-23 17:56:19 -0500

    Continued simplifying heatmap.py

--- scripts/plotly-examples/heatmap.py
+++ scripts/plotly-examples/heatmap.py
@@ -2,81 +2,88 @@ import plotly.offline as py
 import numpy as np           
 from scipy.io import netcdf  
 import warnings
-from numpy import pi, sin, cos
-from IPython.core.display import HTML
 from helpers import *
-import sys
 
 np.set_printoptions(suppress=True,linewidth=80,threshold=80)
 
 class PlotlyStuff:
-    def layout_helper():
-        # Set the plot layout:
-        noaxis=dict(
-            showbackground=False,
-            showgrid=False,
-            showline=False,
-            showticklabels=False,
-            ticks='',
-            title='',
-            zeroline=False
-            )
 
-        aspectratio=dict(x=1,y=1,z=1)
-        eye=dict(x=1.15, y=1.15, z=1.15)
-        camera=dict(eye=eye)
-        font=dict(family='Balto', size=14)
-
-        scene=dict(
-            xaxis=noaxis, 
-            yaxis=noaxis, 
-            zaxis=noaxis,
-            aspectratio=aspectratio,
-            camera=camera
+    # Set the plot layout:
+    noaxis=dict(
+        showbackground=False,
+        showgrid=False,
+        showline=False,
+        showticklabels=False,
+        ticks='',
+        title='',
+        zeroline=False
         )
 
-        layout3d=dict(
-            title='Outgoing Longwave Radiation Anomalies<br>Dec 2017-Jan 2018',
-            font=font,
-            width=800, 
-            height=800,
-            scene=scene,
-            paper_bgcolor='rgba(235,235,235, 0.9)'  
-            )
+    aspectratio=dict(x=1,y=1,z=1)
+    eye=dict(x=1.15, y=1.15, z=1.15)
+    camera=dict(eye=eye)
+    font=dict(family='Balto', size=14)
 
-        return layout3d
+    scene=dict(
+        xaxis=noaxis, 
+        yaxis=noaxis, 
+        zaxis=noaxis,
+        aspectratio=aspectratio,
+        camera=camera
+    )
 
-    layout = layout_helper()
-    
-    colorscale=[[0.0, '#313695'],
-        [0.07692307692307693, '#3a67af'],
-        [0.15384615384615385, '#5994c5'],
-        [0.23076923076923078, '#84bbd8'],
-        [0.3076923076923077, '#afdbea'],
-        [0.38461538461538464, '#d8eff5'],
-        [0.46153846153846156, '#d6ffe1'],
-        [0.5384615384615384, '#fef4ac'],
-        [0.6153846153846154, '#fed987'],
-        [0.6923076923076923, '#fdb264'],
-        [0.7692307692307693, '#f78249'],
-        [0.8461538461538461, '#e75435'],
-        [0.9230769230769231, '#cc2727'],
-        [1.0, '#a50026']]
+    layout=dict(
+        title='Outgoing Longwave Radiation Anomalies<br>Dec 2017-Jan 2018',
+        font=font,
+        width=800, 
+        height=800,
+        scene=scene,
+        paper_bgcolor='rgba(235,235,235, 0.9)'  
+        )
 
-    colorbar=dict(thickness=20, len=0.75, ticklen=4, title= 'W/m²')
+    def sphere(xs, ys, zs, olr, clons, clats, ncolumns, nrows):
 
-    # Define the text to be displayed on hover:
-    def hovertext(clons, clats, OLR, ncolumns, nrows):
-        text = [
+        colorscale=[[0.0, '#313695'],
+            [0.07692307692307693, '#3a67af'],
+            [0.15384615384615385, '#5994c5'],
+            [0.23076923076923078, '#84bbd8'],
+            [0.3076923076923077, '#afdbea'],
+            [0.38461538461538464, '#d8eff5'],
+            [0.46153846153846156, '#d6ffe1'],
+            [0.5384615384615384, '#fef4ac'],
+            [0.6153846153846154, '#fed987'],
+            [0.6923076923076923, '#fdb264'],
+            [0.7692307692307693, '#f78249'],
+            [0.8461538461538461, '#e75435'],
+            [0.9230769230769231, '#cc2727'],
+            [1.0, '#a50026']]
+
+        colorbar=dict(thickness=20, len=0.75, ticklen=4, title= 'W/m²')
+
+        # Define the text to be displayed on hover:
+        hovertext = [
             ['lon: ' +
                 '{:.2f}'.format(clons[i,j]) +
                 '<br>lat: ' +
                 '{:.2f}'.format(clats[i, j])+
                 '<br>W: ' +
-                '{:.2f}'.format(OLR[i][j])
+                '{:.2f}'.format(olr[i][j])
                 for j in range(ncolumns)]
             for i in range(nrows)]
-        return text
+
+        return dict(
+            type='surface',
+            x=xs, 
+            y=ys, 
+            z=zs,
+            colorscale=colorscale,
+            surfacecolor=olr,
+            cmin=-20, 
+            cmax=20,
+            colorbar=colorbar,
+            text=hovertext,
+            hoverinfo='text'
+            )
 
 # Read data from a `netCDF` file:
 with warnings.catch_warnings():
@@ -125,19 +132,10 @@ OLR=np.zeros(clons.shape, dtype=np.float64)
 OLR[:, :ncolumns-1]=np.copy(np.array(olr,  dtype=np.float64))
 OLR[:, ncolumns-1]=np.copy(olr[:, 0])
 
-sphere=dict(
-    type='surface',
-    x=XS, 
-    y=YS, 
-    z=ZS,
-    colorscale=PlotlyStuff.colorscale,
-    surfacecolor=OLR,
-    cmin=-20, 
-    cmax=20,
-    colorbar=PlotlyStuff.colorbar,
-    text=PlotlyStuff.hovertext(clons, clats, OLR, ncolumns, nrows),
-    hoverinfo='text'
+
+fig=dict(
+    data=PlotlyStuff.sphere(XS, YS, ZS, OLR, clons, clats, ncolumns, nrows), 
+    layout=PlotlyStuff.layout
     )
 
-fig=dict(data=sphere, layout=PlotlyStuff.layout)
 py.plot(fig, filename="heatmap.html")
